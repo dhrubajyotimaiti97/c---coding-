f5358c8eee6b19f839ce03a4e36bb4104b542bfb
DHRUBAECE063 <dhrubajyotimaiti97@gmail.com>
2026-03-19 15:15:15 +0530

    Add pattern printing logic in pattern 2.c
    Implement a pattern printing program in C.

---
+++ pattern 2 .c
@@ -0,0 +1,28 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C c
+    int rows=5;
+    for(int i=1 ; i<=5 ; i++){
+        int num;
+        if (i%2 == 0){
+            num=2;
+        }
+            else{
+                num=1;
+                
+            }
+            for(int j=1;j<=i;j++){
+                printf("%d\t",num);
+                num+=2;
+                
+            }
+                
+    printf("\n");
+    
+    }
+
+
+   return 0;
+}
